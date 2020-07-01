4e5be155227e0de9e6d7ebde142d960d3de45616
Eric Fitz <57112072+ekfitz@users.noreply.github.com>
2020-06-30 21:20:27 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
-var homeButton = document.getElementById("home-button");
-var w = window.innerWidth;
-var widthText = document.getElementById("aboutLink");
-widthText.innerHTML = w;
-var stego = document.getElementById("stego");
+hideDetails();
+
+homeButton.onclick = underConstruction;
+
+function underConstruction() {
+  alert("Coming soon. Site under construction. Check back later probably");
+}
+
+closeButton.onclick = hideDetails;
+
+lpButton.deets = lpDetails;
+staticButton.deets = staticDetails;
+dynamicButton.deets = dynamicDetails;
+
+lpButton.onclick = showDetails;
+staticButton.onclick = showDetails;
+dynamicButton.onclick = showDetails;
 
 
-//stego responsive breaks
-function moveStego() {
-  if (w < 990){
-    stego.style.top = 0em;
+
+function hideDetails(){
+  $(".mission").css("display", "none");
+  closeButton.style.display = "none";
+}
+
+function showDetails(){
+  closeButton.style.display = "block";
+  this.deets.style.display = "block";
+  if (this == lpButton) {
+    staticDetails.style.display = "none";
+    dynamicDetails.style.display = "none";
+  } else if (this == staticButton) {
+    lpDetails.style.display = "none";
+    dynamicDetails.style.display = "none";
+  } else if (this == dynamicButton) {
+    lpDetails.style.display = "none";
+    staticDetails.style.display = "none";
   }
 }
