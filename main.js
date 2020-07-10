c63952ddaaa27c6c3ebdd8bca6ad63d4189e39ce
Eric Fitz <57112072+ekfitz@users.noreply.github.com>
2020-07-10 15:34:15 -0700

    Updated responsive breakpoints in js

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
+//1200: arrow too big
+//1000: just right
+//770: arrow & websites card cut off
+//575: all terrible. This is where mobile starts
+
+var jumbotron = document.querySelector(".jumbotron");
+var appleCard = document.querySelector(".advert");
+formTitle.style.fontSize = "2.5em";
+googleForm.style.width = "30em";
+window.addEventListener("resize", responsiveFunction);
 hideDetails();
+responsiveFunction();
+
+function tinyJumbo() {
+  topSection.style.backgroundSize = "100%";
+  topSection.style.backgroundPosition = "0 0";
+  jumbotron.style.top = "15em";
+  jumbotron.style.width = "100%";
+  jumbotron.style.float = "none";
+  jumbotron.style.textAlign = "center";
+}
+
+function returnJumbo() {
+  topSection.style.backgroundSize = "60%";
+  topSection.style.backgroundPosition = "-10em 10em";
+  jumbotron.style.top = "10em";
+  jumbotron.style.width = "55%";
+  jumbotron.style.float = "right";
+  jumbotron.style.textAlign = "right";
+}
+
 homeButton.onclick = underConstruction;
 
 function underConstruction() {
@@ -22,6 +52,31 @@ function hideDetails(){
   $(".hiding").css("height", "0em");
   closeButton.style.display = "none";
 }
+
+
+function responsiveFunction() {
+  var screenWidth = window.innerWidth;
+
+  if (screenWidth > 1195){
+    arrowArrow.style.width = "30%";
+  } else if (screenWidth > 1000){
+    arrowArrow.style.width = "40%";
+    appleCard.style.display = "inline-block";
+  } else if (screenWidth > 770){
+    returnJumbo();
+    googleForm.style.width = "30em";
+    formTitle.style.fontSize = "2.5em";
+    arrowArrow.style.width = "30%";
+    arrowArrow.style.display = "inline-block";
+    appleCard.style.display = "none";
+  } else if (screenWidth < 770){
+    tinyJumbo();
+    googleForm.style.width = "100%";
+    formTitle.style.fontSize = "1.5em";
+    arrowArrow.style.display = "none";
+  }
+}
+
 
 function showDetails(){
   closeButton.style.display = "block";
